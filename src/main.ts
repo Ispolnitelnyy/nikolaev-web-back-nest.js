76cf8529d69712984140462c951b91cbc4fb03d3
unknown <nikolaev3005@gmaol.com>
2023-11-03 11:56:47 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-  app.enableCors({ credentials: true, origin: true });
-  await app.listen(3001);
+    const app = await NestFactory.create(AppModule, { cors: false });
+    app.enableCors({ credentials: true, origin: true });
+
+    const config = new DocumentBuilder()
+        .setTitle("Nikolaev-web-back")
+        .setDescription("The Nikolaev-web-back API description")
+        .setVersion("1.0")
+        .addTag("Nikolaev-web-back")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup("swagger", app, document);
+
+    await app.listen(3001);
 }
 bootstrap();
