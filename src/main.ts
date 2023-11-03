f3c571479bd4a73bb4e9483cbcba626765e1b7bc
unknown <nikolaev3005@gmaol.com>
2023-11-03 10:17:34 +0300

    create entities: users

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
   app.enableCors({ credentials: true, origin: true });
-  await app.listen(5000);
+  await app.listen(3001);
 }
 bootstrap();
