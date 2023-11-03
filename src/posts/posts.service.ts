ef57dc7e6ebdd90cbddf50d41862c1f4831b7068
unknown <nikolaev3005@gmaol.com>
2023-11-03 21:12:45 +0300

    first get all posts done

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Injectable } from "@nestjs/common";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+const { Post } = require("../../database/models");
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+    create(createPostDto: CreatePostDto) {
+        return "This action adds a new post";
+    }
 
-  findAll() {
-    return `This action returns all posts`;
-  }
+    async findAll() {
+        try {
+            const allPosts = await Post.findAll({
+                order: [["createdAt", "DESC"]]
+            });
+            return allPosts;
+        } catch (error) {
+            console.log(error);
+            throw new Error("Failed to fetch posts");
+        }
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} post`;
+    }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+    update(id: number, updatePostDto: UpdatePostDto) {
+        return `This action updates a #${id} post`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} post`;
+    }
 }
